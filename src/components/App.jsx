ecbed2b60e7c491ceb6c9a7c24d071ee6ed52101
AvirukBasak <avirukbasak@gmail.com>
2023-09-20 18:57:38 +0530

    reorganized dir structure w/ webpack

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import printHelloWorld from '@/scripts/script';
 
 import '@/styles/global.css';
+import AppStyles from '@/styles/App.module.css';
 
 export default class App extends React.Component {
   render() {
     printHelloWorld();
     return (
-      <>
+      <div className={AppStyles.background}>
         <h1>Demo App</h1>
         <p>This is a Paragraph</p>
-      </>
+      </div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 const path = require('path');
 const HWP = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: path.join(__dirname, '/src/index.js'),
     resolve: {
         extensions: ['', '.js', '.jsx', '.json'],
         alias: {
-            '@': path.resolve(__dirname, "src"),
+            '@': [
+                path.resolve(__dirname, "src"),
+                path.resolve(__dirname, "public"),
+            ]
         },
     },
     output: {
         filename: 'build.js',
-        path: path.join(__dirname, '/build')
+        path: path.join(__dirname, '/build'),
     },
     module: {
         rules: [
@@ -31,6 +35,9 @@ module.exports = {
         ]
     },
     plugins: [
-        new HWP({ template: path.join(__dirname, '/src/index.html') })
+        new HWP({ template: path.join(__dirname, '/src/index.html') }),
+        new CopyWebpackPlugin({
+            patterns: [{ from: path.join(__dirname, '/public') }]
+        }),
     ]
 }
