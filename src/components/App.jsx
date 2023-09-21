c78cc990c002a0026a7a59c0af6f653450ee1b8e
AvirukBasak <avirukbasak@gmail.com>
2023-09-21 17:54:17 +0530

    minor changes to template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import printHelloWorld from '@/scripts/script';
 import styles from '@/styles/App.module.css';
 
-export default class App extends React.Component {
-  render() {
-    printHelloWorld();
-    return (
-      <>
-        <div className={styles.background}>
-          <h1>Demo App</h1>
-          <p>This is a Paragraph</p>
-          <img src="/images/background.jpg"/>
-        </div>
-      </>
-    );
-  }
+export default function App({ page }) {
+  printHelloWorld();
+  return (
+    <>
+      <div className={styles.app}>
+        <h1>Demo App</h1>
+        <p>This is a Paragraph</p>
+        <img src="/images/background.jpg" />
+      </div>
+    </>
+  );
 }
