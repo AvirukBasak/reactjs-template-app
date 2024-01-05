0effebc9f1ea90431e2e960a1f746076d948ac4f
AvirukBasak <avirukbasak@gmail.com>
2024-01-05 16:52:43 +0530

    added typescript, eslint, tailwind

---
+++ src/components/App/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { printHelloWorld } from '@/utils/script'
+
+import background from '@images/background.jpg'
+import styles from './App.module.css'
+
+const App = (): JSX.Element => {
+  printHelloWorld()
+  return (
+    <div className={styles.app}>
+      <h1>Demo App</h1>
+      <p>This is a Paragraph</p>
+      <img src={background} />
+    </div>
+  )
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,120 @@
 /* If you do not know webpack, DO NOT TOUCH this file.
-   IDK webpack so I don't know 80% of WTF is going on. */
+   IDK webpack so I don't know 80% of what's is going on. */
 
 /* This file is in CommonJS syntax.
    Converting it into ES modules syntax MAY NOT work. */
 
-const path = require('path');
-const HWP = require('html-webpack-plugin');
-const CWP = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-/* list of paths of this project */
-const Paths = {
-    src: path.join(__dirname, 'src'),          // jsx, styles and browser js
-    build: path.join(__dirname, '../static'),  // output can be served statically
-    public: path.join(__dirname, 'public'),    // public resources
+const isProduction = process.env.NODE_ENV == "production";
+
+/** list of paths of this project */
+const PATHS = {
+    src: path.join(__dirname, "src"),          // jsx, styles and browser js
+    build: path.join(__dirname, "dist"),       // output can be served statically
+    public: path.join(__dirname, "public"),    // public resources
 };
 
-module.exports = {
-    /* webpack entry point */
-    entry: path.join(Paths.src, 'index.js'),
-    resolve: {
-        /* no idea why I put it and at this point am too
-           afraid to remove it */
-        extensions: ['', '.js', '.jsx', '.json'],
-        /* the magic that allows accessing public res via `/`
-           and src files via `@/` prefixes */
-        alias: {
-            '@public': Paths.public,
-            '@': Paths.src,
+/** webpack mode */
+const wpcMode = isProduction ? "producion" : "development";
+
+/** webpack entry point */
+const wpcEntry = path.join(PATHS.src, "index");
+
+const wpcResolve = {
+    /* no idea why I put it and at this point am too
+       afraid to remove it */
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json", ""],
+    /* the magic that allows importing via "@" prefix */
+    alias: {
+        "@": PATHS.src,
+        "@components": path.join(PATHS.src, "components"),
+        "@contexts": path.join(PATHS.src, "contexts"),
+        "@fonts": path.join(PATHS.src, "fonts"),
+        "@images": path.join(PATHS.src, "images"),
+        "@pages": path.join(PATHS.src, "pages"),
+        "@styles": path.join(PATHS.src, "styles"),
+        "@utils": path.join(PATHS.src, "utils"),
+        "@public": PATHS.public,
+    },
+};
+
+/* webpack build directory and bundle script name.
+   the bundle script is the result of compiling
+   all js and jsx code into one js script.
+   this compilation is done by webpack and babel. */
+const wpcOutput = {
+    path: PATHS.build,
+};
+
+/* to prevent a warning coz bundle size exceeds a certain limit */
+const wpcPerformance = {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+};
+
+/* webpack dev server config */
+const wpcDevServer = {
+    port: 3000,
+    host: "0.0.0.0",
+    compress: true,
+    allowedHosts: "all",
+    hot: true
+};
+
+const wpcModule = {
+    rules: [
+        /* loader rules for css files */
+        {
+            test: /\.css$/,
+            use: [
+                { loader: "style-loader" },
+                { loader: "css-loader", options: { modules: true } },
+            ]
         },
-    },
-    /* webpack build directory and bundle script name.
-       the bundle script is the result of compiling
-       all js and jsx code into one js script.
-       this compilation is done by webpack and babel. */
-    output: {
-        filename: '[name].bundle.js',
-        path: Paths.build,
-    },
-    /* to prevent a warning coz bundle size exceeds a certain limit */
-    performance: {
-        hints: false,
-        maxEntrypointSize: 512000,
-        maxAssetSize: 512000
-    },
-    module: {
-        rules: [
-            /* loader rules for jsx and js files */
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            /* loader rules for css files */
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            /* loader rules for public resource type files viz. images
-               and fonts are not added so that they be accessed wrt `/` */
-        ]
-    },
-    plugins: [
-        /* browser entry point html, hooked up with *.bundle.js */
-        new HWP({ template: path.join(Paths.src, 'index.html') }),
-        /* copy public resources into build dir, otherwise you cannot
-           access the public resources in browser */
-        new CWP({
-            patterns: [
-                { from: Paths.public, to: Paths.build },
-            ],
-        }),
+        /* assets */
+        {
+            test: /\.(eot|svg|ttf|woff|woff2|png|jpe?g|jp2|gif)$/i,
+            type: "asset/resource",
+        },
+        /* exclude node_modules from ts-loader */
+        {
+            test: /\.(ts|tsx)$/i,
+            loader: "ts-loader",
+            exclude: ["/node_modules/"],
+        },
+        /* loader rules for jsx and js files */
+        {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+        },
     ]
-}
+};
+
+const wpcPlugins = [
+    /* browser entry point html, hooked up with *.bundle.js */
+    new HtmlWebpackPlugin({ template: path.join(PATHS.src, "index.html") }),
+
+    /* copy public resources into build dir, otherwise you cannot
+       access the public resources in browser */
+    new CopyWebpackPlugin({
+        patterns: [
+            { from: PATHS.public, to: PATHS.build },
+        ],
+    }),
+];
+
+module.exports = {
+    mode: wpcMode,
+    entry: wpcEntry,
+    resolve: wpcResolve,
+    output: wpcOutput,
+    performance: wpcPerformance,
+    devServer: wpcDevServer,
+    module: wpcModule,
+    plugins: wpcPlugins,
+};
