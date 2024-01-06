9c1ba558b252616b26ec6b60539be6ea1c6c15ee
AvirukBasak <avirukbasak@gmail.com>
2024-01-07 00:45:47 +0530

    jest: added mocking of react assets imports

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
   transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
   ],
+  moduleNameMapper: {
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/mocks/fileMock.ts",
+    "\\.(css|less)$": "<rootDir>/src/mocks/fileMock.ts",
+    "@/(.*)": "<rootDir>/src/$1",
+    "@public/(.*)": "<rootDir>/public/$1",
+  }
 };
