e590cb2612a99c1b078207bed306828c6da1bd69
AvirukBasak <avirukbasak@gmail.com>
2024-01-07 14:45:59 +0530

    updated index.tsx

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -7,8 +7,8 @@ import styles from './App.module.css'
 const App = (): JSX.Element => {
   printHelloWorld()
   return (
-    <div className={styles.app + ' font-bold'}>
-      <h1>Demo App</h1>
+    <div className={styles.app}>
+      <h1 className="font-bold">Demo App</h1>
       <p>This is a Paragraph</p>
       <img src={background} />
     </div>
